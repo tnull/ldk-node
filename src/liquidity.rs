bd9b06de67b1ad29e60f82f33a290ec3338452d1
Elias Rohrer <dev@tnull.de>
2024-01-08 08:30:04 +0100

    Implement LSPS2 API methods

--- src/liquidity.rs
+++ src/liquidity.rs
@@ -81,6 +81,10 @@ where
 		self.liquidity_manager.as_ref()
 	}
 
+	pub(crate) fn get_liquidity_source_details(&self) -> Option<(PublicKey, SocketAddress)> {
+		self.lsps2_service.as_ref().map(|s| (s.node_id, s.address.clone()))
+	}
+
 	pub(crate) async fn handle_next_event(&self) {
 		match self.liquidity_manager.next_event_async().await {
 			Event::LSPS2Client(LSPS2ClientEvent::OpeningParametersReady {
