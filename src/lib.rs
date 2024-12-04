0f9d105180223fec6457c8afa03632054eb76044
Elias Rohrer <dev@tnull.de>
2024-12-04 14:02:26 +0100

    Add LSPS1 API
    We add an `Lsps1Liquidity` API object, mirroring the approach we took
    with the `payment` APIs.

--- src/lib.rs
+++ src/lib.rs
@@ -84,7 +84,7 @@ mod gossip;
 pub mod graph;
 mod hex_utils;
 pub mod io;
-mod liquidity;
+pub mod liquidity;
 mod logger;
 mod message_handler;
 pub mod payment;
@@ -100,6 +100,7 @@ pub use bip39;
 pub use bitcoin;
 pub use lightning;
 pub use lightning_invoice;
+pub use lightning_liquidity;
 pub use vss_client;
 
 pub use balance::{BalanceDetails, LightningBalance, PendingSweepBalance};
@@ -129,7 +130,7 @@ use event::{EventHandler, EventQueue};
 use gossip::GossipSource;
 use graph::NetworkGraph;
 use io::utils::write_node_metrics;
-use liquidity::LiquiditySource;
+use liquidity::{LiquiditySource, Lsps1Liquidity};
 use payment::store::PaymentStore;
 use payment::{
 	Bolt11Payment, Bolt12Payment, OnchainPayment, PaymentDetails, SpontaneousPayment,
@@ -956,6 +957,34 @@ impl Node {
 			self.bolt11_payment(),
 			self.bolt12_payment(),
 			Arc::clone(&self.config),
+			Arc::clone(&self.logger),
+		))
+	}
+
+	/// Returns a liquidity handler allowing to request channels via the [LSPS1] protocol.
+	///
+	/// [LSPS1]: https://github.com/BitcoinAndLightningLayerSpecs/lsp/tree/main/LSPS1
+	#[cfg(not(feature = "uniffi"))]
+	pub fn lsps1_liquidity(&self) -> Lsps1Liquidity {
+		Lsps1Liquidity::new(
+			Arc::clone(&self.runtime),
+			Arc::clone(&self.wallet),
+			Arc::clone(&self.connection_manager),
+			self.liquidity_source.clone(),
+			Arc::clone(&self.logger),
+		)
+	}
+
+	/// Returns a liquidity handler allowing to request channels via the [LSPS1] protocol.
+	///
+	/// [LSPS1]: https://github.com/BitcoinAndLightningLayerSpecs/lsp/tree/main/LSPS1
+	#[cfg(feature = "uniffi")]
+	pub fn lsps1_liquidity(&self) -> Arc<Lsps1Liquidity> {
+		Arc::new(Lsps1Liquidity::new(
+			Arc::clone(&self.runtime),
+			Arc::clone(&self.wallet),
+			Arc::clone(&self.connection_manager),
+			self.liquidity_source.clone(),
 			Arc::clone(&self.logger),
 		))
 	}
