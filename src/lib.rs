05823920faae2db29c06b3085ebe072248d1f131
Elias Rohrer <dev@tnull.de>
2025-05-23 13:59:59 +0200

    f oops

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// This file is Copyright its original authors, visible in version control history.lib
+// This file is Copyright its original authors, visible in version control history.
 //
 // This file is licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
